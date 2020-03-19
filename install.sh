c0258d15e22ee2597ba36229aecdf1f6bd8c7942
Adrian W. Roque <adrian.w.roque@gmail.com>
2020-03-18 22:41:55 -0300

    Create install.sh
    Simple shell script to configure your dotfiles repo to sync your configurations across UNIX machines. Tested only on Linux, for now.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Simple shell script to configure your dotfiles
+# repository to sync your configurations across
+# UNIX machines. Tested only on Linux, for now.
+
+git clone --bare git@github.com:AdrianWR/Dotfiles.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files to $HOME/.config-backup.";
+    config checkout 2>&1 | egrep "\s+\." | tr :-d '\t' | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
